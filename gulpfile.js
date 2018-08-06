2ee4a028fb4730b838b0a46e2deb7cc106760fc5
@zhangpei001 <1635728114@qq.com>
2018-08-06 08:52:44 +0800

    8.6这是版本2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+// var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var connect = require("gulp-connect");
@@ -7,36 +7,36 @@ var connect = require("gulp-connect");
 // 定义一个复制文件的任务
 gulp.task("copyfile",function(){
 	gulp.src("*.html")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project"));
 });
 // 复制JS文件
 gulp.task("copyjs",function(){
 	gulp.src("js/*.js")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\js"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\js"));
 });
 // 复制css文件
 gulp.task("copycss",function(){
 	gulp.src("css/*.css")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\css"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\css"));
 });
 // 复制图片文件
 gulp.task("images",function(){
 	gulp.src("images/*.*")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\img"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\img"));
 });
 
 //sass编译
-gulp.task("sass",function(){
-	gulp.src(["css/*.scss"])
-	.pipe(sass())
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\css"));
-});
+// gulp.task("sass",function(){
+// 	gulp.src(["css/*.scss"])
+// 	.pipe(sass())
+// 	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\css"));
+// });
 
 //合并文件
 gulp.task("concatjs",function(){
 	gulp.src(["js/index.js","js/goodslist.js"])
 	.pipe(concat("index.js"))
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\js"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\js"));
 });
 
 
@@ -45,7 +45,7 @@ gulp.task("concatanduglifyjs",function(){
 	gulp.src(["js/index.js","js/goodslist.js"])
 	.pipe(concat("index.js"))
 	.pipe(uglify())
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\MEIZU\\js"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\project\\js"));
 });
 // 启动监听器
 gulp.task("watchall",function(){
@@ -53,6 +53,6 @@ gulp.task("watchall",function(){
 	gulp.watch("js/*.js",["copyjs"]);
 	gulp.watch("css/*.css",["copycss"]);
 	gulp.watch("img/*.*",["images"]);
-	gulp.watch("css/*.scss",["sass"]);
+	// gulp.watch("css/*.scss",["sass"]);
 	gulp.watch("js/index.js",["concatanduglifyandrenamejs"]);
 });//当文件发生变化时执行这个命令
